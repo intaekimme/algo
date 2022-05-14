31334e57008c1598bcb839b7c10423023c1022e8
InTae <kit938639@gmail.com>
2022-05-14 17:29:11 +0900

    Upload Main_BOJ_1918_후위표기식_G3_76ms_Stack

--- 0514/src/Main_BOJ_1918_후위표기식_.java
+++ 0514/src/Main_BOJ_1918_후위표기식_.java
@@ -15,68 +15,40 @@ import java.util.Stack;
  *
  */
 public class Main_BOJ_1918_후위표기식_ {
-
-	static class Op{
-		char op;
-		int pr;
-		
-		public Op(char op, int pr) {
-			super();
-			this.op = op;
-			this.pr = pr;
-		}
-		
-	}
+	
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		String exp = br.readLine();
-		Stack<Op> stack = new Stack<Op>();
+		char[] exp = br.readLine().toCharArray();
+		Stack<Character> stack = new Stack<Character>();
 		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<exp.length(); i++) {
-			char s = exp.charAt(i);
-			int pr = check(s);
-			
-			if(pr == -2) {
-				while(!stack.isEmpty()) {
-					char tmp = stack.pop().op;
-					if(tmp != '(') {
-						sb.append(tmp);
-					}
+		for(int i=0; i<exp.length; i++) {
+			if('A' <= exp[i] && exp[i] <= 'Z') {
+				sb.append(exp[i]);
+				continue;
+			}
+			if(exp[i] == '(') {
+				stack.push('(');
+				continue;
+			}
+			if(exp[i] == ')') {
+				while(!stack.isEmpty() && stack.peek() != '(') {
+					sb.append(stack.pop());
 				}
+				if(!stack.isEmpty()) stack.pop();
 				continue;
 			}
-			if(pr==-1) {
-				sb.append(s);
-				continue;
+			while(!stack.isEmpty() && check(stack.peek()) >= check(exp[i])) {
+				sb.append(stack.pop());
 			}
-			if(pr == 0) {
-				stack.push(new Op(s, pr));
-				continue;
-			}
-			if(stack.isEmpty()) {
-				stack.push(new Op(s, pr));
-			}else {
-				Op cur = stack.peek();
-				if(cur.pr < pr) {
-					stack.push(new Op(s, pr));
-				}else {
-					while(!stack.isEmpty() && cur.pr >= pr) {
-						char tmp = stack.pop().op;
-						if(tmp != '(') {
-							sb.append(tmp);
-						}
-					}
-					stack.push(new Op(s, pr));
-				}
-			}
-		}	//	end of for
-		while(!stack.isEmpty()) {
-			sb.append(stack.pop().op);
+			stack.push(exp[i]);
 		}
-		System.out.println(sb);
+		while(!stack.isEmpty()) {
+			sb.append(stack.pop());
+		}
 		
+		System.out.println(sb);
 	}
 
 	private static int check(char s) {
@@ -87,12 +59,8 @@ public class Main_BOJ_1918_후위표기식_ {
 		case '+':		
 		case '-':
 			return 1;
-		case '(':
-			return 0;
-		case ')':
-			return -2;
 		default:
-			return -1;
+			return 0;
 		}
 	}
 
