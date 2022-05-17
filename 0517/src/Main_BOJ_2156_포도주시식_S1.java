a5a0b61d90d48986e18615a3056c310b7f63a1c9
InTae <kit938639@gmail.com>
2022-05-18 00:45:16 +0900

    Main_BOJ_2156_포도주시식_S1_112ms

--- 0517/src/Main_BOJ_2156_포도주시식_S1.java
+++ 0517/src/Main_BOJ_2156_포도주시식_S1.java
@@ -20,28 +20,28 @@ public class Main_BOJ_2156_포도주시식_S1 {
 //		StringTokenizer st = new StringTokenizer(br.readLine());
 //		N = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(br.readLine());
-		arr = new int[N+1];
+		arr = new int[N];
 //		int idx=1;
 //		while(st.hasMoreTokens()) {
 //			arr[idx++] = Integer.parseInt(st.nextToken());
 //		}
 		
-		for(int i=1; i<=N; i++) {
+		for(int i=0; i<N; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
 		
-		if(N<=3) {
+		if(N<3) {
 			int ans = 0;
-			for(int i=1; i<=N; i++) {
+			for(int i=0; i<N; i++) {
 				ans += arr[i];;
 			}
 			System.out.println(ans);
 			return;
 		}
-		dp = new int[N+1];
-		dp[1] = arr[1];
-		dp[2] = dp[1] + arr[2];
-		dp[3] = Math.max(dp[2], Math.max(arr[2] + arr[3], dp[1] + arr[3]));
+		dp = new int[N];
+		dp[0] = arr[0];
+		dp[1] = dp[0] + arr[1];
+		dp[2] = Math.max(dp[1], Math.max(arr[1] + arr[2], dp[0] + arr[2]));
 		
 //		StringBuilder sb = new StringBuilder();
 //		for(int i=0; i<=N; i++) {
@@ -49,7 +49,7 @@ public class Main_BOJ_2156_포도주시식_S1 {
 //		}
 //		sb.append("\n");
 		
-		for(int i=3; i<=N; i++) {
+		for(int i=3; i<N; i++) {
 			dp[i] = Math.max(dp[i-3] + arr[i-1] + arr[i], dp[i-2]+arr[i]);
 			dp[i] = Math.max(dp[i-1], dp[i]);
 		}
@@ -58,7 +58,7 @@ public class Main_BOJ_2156_포도주시식_S1 {
 //		}
 //		
 //		System.out.println(sb);
-		System.out.println(dp[N]);
+		System.out.println(dp[N-1]);
 
 		
 	}
