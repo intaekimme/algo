c23fdde355226ffdd0159a37cb61dfbd6437846a
InTae <kit938639@gmail.com>
2022-05-18 00:44:20 +0900

    Main_BOJ_2156_포도주시식_S1_fail2

--- 0517/src/Main_BOJ_2156_포도주시식_S1.java
+++ 0517/src/Main_BOJ_2156_포도주시식_S1.java
@@ -13,67 +13,53 @@ import java.util.StringTokenizer;
 public class Main_BOJ_2156_포도주시식_S1 {
 
 	private static int N;
-	private static int[] arr;
-	private static int[][] dp;
+	private static int[] arr, dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-//		N = Integer.parseInt(br.readLine());
-		arr = new int[N];
-		dp = new int[3][N];
-		int idx=1;
-		while(st.hasMoreTokens()) {
-			arr[idx++] = Integer.parseInt(st.nextToken());
-		}
-		
-//		for(int i=1; i<=N; i++) {
-//			arr[i] = Integer.parseInt(br.readLine());
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+//		N = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N+1];
+//		int idx=1;
+//		while(st.hasMoreTokens()) {
+//			arr[idx++] = Integer.parseInt(st.nextToken());
 //		}
 		
-		for(int i=0; i<3; i++) {
-			for(int j=1; j<=N; j++) {
-				if(i==0) {
-					if(j%3!=0) {
-						dp[i][j] = dp[i][j-1] + arr[j];
-					}else {
-						dp[i][j] = dp[i][j-1];
-					}
-					continue;
-				}
-				if(i==1) {
-					if(j%3!=2) {
-						dp[i][j] = dp[i][j-1] + arr[j];
-					}else {
-						dp[i][j] = dp[i][j-1];
-					}
-					continue;
-				}
-				if(i==2) {
-					if(j%3!=1) {
-						dp[i][j] = dp[i][j-1] + arr[j];
-					}else {
-						dp[i][j] = dp[i][j-1];
-					}
-					continue;
-				}
-			}
+		for(int i=1; i<=N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
 		}
 		
-		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<=N; i++) {
-			sb.append(arr[i]).append("\t");
-		}
-		sb.append("\n");
-		for(int i=0; i<3; i++) {
-			for(int j=0; j<=N; j++) {
-				sb.append(dp[i][j]).append("\t");
+		if(N<=3) {
+			int ans = 0;
+			for(int i=1; i<=N; i++) {
+				ans += arr[i];;
 			}
-			sb.append("\n");
+			System.out.println(ans);
+			return;
 		}
-		System.out.println(sb);
-		System.out.println(Math.max(Math.max(dp[0][N],dp[1][N]), dp[2][N]));
+		dp = new int[N+1];
+		dp[1] = arr[1];
+		dp[2] = dp[1] + arr[2];
+		dp[3] = Math.max(dp[2], Math.max(arr[2] + arr[3], dp[1] + arr[3]));
+		
+//		StringBuilder sb = new StringBuilder();
+//		for(int i=0; i<=N; i++) {
+//			sb.append(arr[i]).append("\t");
+//		}
+//		sb.append("\n");
+		
+		for(int i=3; i<=N; i++) {
+			dp[i] = Math.max(dp[i-3] + arr[i-1] + arr[i], dp[i-2]+arr[i]);
+			dp[i] = Math.max(dp[i-1], dp[i]);
+		}
+//		for(int j=0; j<=N; j++) {
+//			sb.append(dp[j]).append("\t");
+//		}
+//		
+//		System.out.println(sb);
+		System.out.println(dp[N]);
+
 		
 	}
 
