596706e9605541bdb1470d23ed9cda72470e5c86
InTae <kit938639@gmail.com>
2022-07-02 19:42:32 +0900

    Upload Main_BOJ_1075_토너먼트_S3_Success

--- 0702/src/Main_BOJ_1075_토너먼트_S3.java
+++ 0702/src/Main_BOJ_1075_토너먼트_S3.java
@@ -5,11 +5,16 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/**
+ * 라운드를 돌며 임한수와 김지민이 가지는 수 중 큰 수가 짝수이고 두 수의 차가 1이면 이번 라운드에서 만난다.
+ * 다음 라운드에서 새로운 순서를 줄 때는 이번 라운드 순서에 1을 더하고 2로 나눠주면 된다.
+ * @author kit938639
+ *
+ */
 public class Main_BOJ_1075_토너먼트_S3 {
 
 	
 	private static int N, jimin, hansu;
-	private static boolean[] player;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,43 +25,17 @@ public class Main_BOJ_1075_토너먼트_S3 {
 		jimin = Integer.parseInt(st.nextToken());
 		hansu = Integer.parseInt(st.nextToken());
 		
-//		player = new boolean[N+1];
-//		player[jimin] = player[hansu] = true;
-		
-		Queue<Integer> tournament = new LinkedList<Integer>();
-		Queue<Integer> tmp = new LinkedList<Integer>();
-		for(int i=1; i<=N; i++) {
-			tournament.add(i);
-		}
-		
 		int cnt = 1;
 		
 		while(true) {
-			if(tournament.size() > 1) {
-				int player1 = tournament.poll();
-				int player2 = tournament.poll();
-				
-				if(player1 == jimin && player2 == hansu) {
-					break;
-				}
-				if(player1 == jimin || player1 == hansu) {
-					tmp.add(player1);
-				}
-				if(player2 == jimin || player2 == hansu) {
-					tmp.add(player2);
-				}
-				if(player1 != jimin && player1 != hansu && player2 != jimin && player2 != hansu) {
-					tmp.add(player1);
-				}
-			}else if(tournament.size() == 1) {
-				tmp.add(tournament.poll());
+			if(Math.abs(jimin - hansu) == 1 && Math.max(jimin, hansu)%2 == 0) {
+				break;
 			}else {
-				while(!tmp.isEmpty()) {
-					tournament.add(tmp.poll());
-				}
+				jimin = (jimin + 1) / 2;
+				hansu = (hansu + 1) / 2;
 				cnt++;
 			}
-		}	//	end of while
+		}
 		
 		System.out.println(cnt);
 	}
